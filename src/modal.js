4e4583e3f1b5e8a5873cdd17be00d901f65af3c4
Irving Bautista <irvingbautista681@gmail.com>
2025-08-04 14:45:48 -0700

    feat: add project modals to allow users to create new projects

--- src/modal.js
+++ src/modal.js
@@ -1,30 +1,52 @@
 import "./styles.css"
-import Todos from "./todos"
+import Todo from "./todos.js"
+import Project from "./project.js"
 
 const task = document.querySelector(".create-task")
-const dialog = document.querySelector("dialog")
-const form = document.querySelector("form")
-const closebtn = document.querySelector(".cancel")
+const taskDialog = document.querySelector(".task-dialog")
+const taskForm = document.querySelector(".task-form")
+const taskCloseBtn = document.querySelector(".task.cancel")
 
+task.addEventListener("click", () => taskDialog.showModal())
 
-task.addEventListener("click", () => dialog.showModal())
-
-closebtn.addEventListener("click", () => {
-    dialog.close()
-    form.reset()
+taskCloseBtn.addEventListener("click", () => {
+    taskDialog.close()
+    taskForm.reset()
 })
 
-form.addEventListener("submit", (e) => {
+taskForm.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    const formData = new FormData(form)
+    const formData = new FormData(taskForm)
     const title = formData.get("task-title")
     const description = formData.get("task-description")
     const date = formData.get("task-date")
     const priority = formData.get("task-priority")
+    const newTask = new Todo(title, description, date, priority)
+    console.log(newTask)
+    taskForm.reset()
+    taskDialog.close()
+})
 
-    const task = new Todos(title, description, date, priority)
-    console.log(task)
-    form.reset()
-    dialog.close()
+const project = document.querySelector(".create-project")
+const projectDialog = document.querySelector(".project-dialog")
+const projectForm = document.querySelector(".project-form")
+const projectCloseBtn = document.querySelector(".project.cancel")
+
+project.addEventListener("click", () => projectDialog.showModal())
+
+projectCloseBtn.addEventListener("click", () => {
+    projectDialog.close()
+    projectForm.reset()
+})
+
+projectForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(projectForm)
+    const projectTitle = formData.get("project-title")
+    const newProject = new Project(projectTitle)
+    console.log(newProject)
+    projectForm.reset()
+    projectDialog.close()
 })
--- src/todos.js
+++ src/todos.js
@@ -1,4 +1,4 @@
-export default class Todos {
+export default class Todo {
     constructor(title, description, dueDate, priority) {
         this.title = title
         this.description = description
